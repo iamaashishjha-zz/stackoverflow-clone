ca5a75a617e565be6a80cb1e64121a0f1bb17734
Aashish Jha <mail.aashishjha@gmail.com>
2020-06-05 08:31:43 +0545

    Display all questions

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                   @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                {{ Str::limit($question->body, 250) }}
+                            </div>
+                        </div>
+                        <hr>
+                   @endforeach
+                        <div class="text-center">
+                            {{ $questions->links() }}
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
