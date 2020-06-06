ee3efec48464dbf01c032a5224c32420266595f6
Aashish Jha <mail.aashishjha@gmail.com>
2020-06-06 17:43:37 +0545

    Implement show question section

---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h1>{{ $question->title }}</h1>
+                        <div class="ml-auto">
+                            <a href="{{ route('question.index') }}" class="btn btn-outline-secondary">Back to all questions</a>
+                        </div>
+                    </div>
+                    </div>
+
+                <div class="card-body">
+                    {!! $question->body_html !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('question', 'QuestionController');
+Route::resource('question', 'QuestionController')->except('show');
+
+Route::get('question/{slug}', 'QuestionController@show')->name('question.show');
